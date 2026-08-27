fc1546320b4e3da13c1adeb36dea891ebae67a20
RAJ VAMAN NAIK <naikraj116@gmail.com>
2026-08-27 22:40:57 +0530

    remotion-documentary: register script-driven episode composition

--- remotion-documentary/src/RemotionRoot.tsx
+++ remotion-documentary/src/RemotionRoot.tsx
@@ -4,6 +4,7 @@ import {TestRender} from "./compositions/TestRender";
 import {Showcase} from "./compositions/Showcase";
 import {DocumentaryShot} from "./compositions/DocumentaryShot";
 import {ProductionDocumentary, PRODUCTION_DOCUMENTARY_SPEC} from "./compositions/ProductionDocumentary";
+import {ScriptEpisodeComposition} from "./compositions/ScriptEpisodeComposition";
 
 export const RemotionRoot: React.FC = () => {
   const productionDuration = PRODUCTION_DOCUMENTARY_SPEC.shots.reduce((sum, shot) => sum + shot.durationInFrames, 0);
@@ -12,6 +13,7 @@ export const RemotionRoot: React.FC = () => {
     <Composition id="DocumentaryShot" component={DocumentaryShot} durationInFrames={150} fps={30} width={1920} height={1080}/>
     <Composition id="Showcase" component={Showcase} durationInFrames={1500} fps={30} width={1920} height={1080}/>
     <Composition id="ProductionDocumentary" component={ProductionDocumentary} durationInFrames={productionDuration} fps={PRODUCTION_DOCUMENTARY_SPEC.fps} width={PRODUCTION_DOCUMENTARY_SPEC.width} height={PRODUCTION_DOCUMENTARY_SPEC.height}/>
+    <Composition id="ScriptEpisode" component={ScriptEpisodeComposition} durationInFrames={900} fps={30} width={1920} height={1080}/>
   </>;
 };
 
